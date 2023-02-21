6ddcbbe8c2c235613bcc56bea193219dce5df0f3
tomgodee <trungvu8295@gmail.com>
2023-02-21 14:51:50 +0700

    Add vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
 import react from "@vitejs/plugin-react";
 
 export default ({ mode }) => {
@@ -7,6 +9,6 @@ export default ({ mode }) => {
 
   return defineConfig({
     base: "/o-an-quan/",
-    plugins: [react()],
+    plugins: [react(), tsconfigPaths()],
   });
 };
